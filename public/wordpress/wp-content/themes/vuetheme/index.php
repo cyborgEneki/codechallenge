2fa454b43a940fb4515c423266a7a6e0ba11588e
Eneki <joanreneki@gmail.com>
2019-05-25 20:46:03 +0300

    add vuetheme and functions file

---
+++ public/wordpress/wp-content/themes/vuetheme/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html <?php language_attributes(); ?>> 
+<head> 
+   <?php wp_head(); ?> 
+</head> 
+
+<body> 
+    //Vue app mounted
+    <div id="app">
+    </div> 
+    <?php wp_footer(); ?> 
+</body> 
+</html>
